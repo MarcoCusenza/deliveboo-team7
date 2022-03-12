d977ff0e2a5bace872aee193f1326ab8c03dd6fa
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-03-12 13:47:02 +0100

    fix redirect after login

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ use Illuminate\Support\Facades\Auth;
 // Auth::routes(["verify" => true]);
 Auth::routes();
 
-Route::get('/', function () {
-  return view('front');
-});
-
 
 Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(
   function () {
-    // Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home', 'HomeController@index')->name('home');
   }
 );
+
+// Route::get('/', function () {
+//   return view('front');
+// });
 
 // router di Vue
 Route::get("{any?}", function () {
