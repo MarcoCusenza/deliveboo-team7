e2a4e6307f6bff84e15913b0921feee511a991b3
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-03-12 11:13:53 +0100

    Pronto UserSeeder per seedare il DB

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = config("filler_files/users-filler");
+
+        foreach($users as $user){
+            $newUser = new User();
+            $newUser->name = $user["name"];
+            $newUser->surname = $user["surname"];
+            $newUser->email = $user["email"];
+            $newUser->password = $user["password"];
+            $newUser->VAT_number = $user["VAT_number"];
+            $newUser->save();
+        }
+    }
+}
