0c2338d8799dbeccbd28895ca0f79ea979d886e9
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-03-12 11:15:48 +0100

    Fill Categories

--- config/filler_files/categories-filler.php
+++ config/filler_files/categories-filler.php
@@ -7,25 +7,61 @@ return [
   [
     "name" => "Giapponese",
     "slug" => "giapponese",
-    "image" => "https://....."
+    "image" => "https://i.imgur.com/WgVikPx.png"
   ],
 
   // id=2
-
+  [
+    "name" => "Carne",
+    "slug" => "carne",
+    "image" => "https://imgur.com/IwWly1r"
+  ],
 
   // id=3
-
+  [
+    "name" => "Pesce",
+    "slug" => "pesce",
+    "image" => "https://imgur.com/BgHUdtu"
+  ],
 
   // id=4
-
+  [
+    "name" => "Italiano",
+    "slug" => "italiano",
+    "image" => "https://imgur.com/WHf7Xwq"
+  ],
 
   // id=5
-
+  [
+    "name" => "Cinese",
+    "slug" => "cinese",
+    "image" => "https://imgur.com/GKOv24W"
+  ],
 
   // id=6
-
+  [
+    "name" => "Indiano",
+    "slug" => "indiano",
+    "image" => "https://imgur.com/gNvqbTe"
+  ],
 
   // id=7
-
+  [
+    "name" => "Messicano",
+    "slug" => "messicano ",
+    "image" => "https://imgur.com/NJzSDI6"
+  ],
+  // id=8
+  [
+    "name" => "Pizza",
+    "slug" => "pizza",
+    "image" => "https://imgur.com/ixKRIem"
+  ],
+  // id=9
+  [
+    "name" => "Hamburger",
+    "slug" => "hamburger",
+    "image" => "https://imgur.com/qeNtx1r"
+  ],
 ]
 ?>
