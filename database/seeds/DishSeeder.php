d469cef2306c012425c2bfea5a68fffc3d2625b0
francesca <fradeluca85@gmail.com>
2022-03-12 11:29:24 +0100

    inizio dish seeder

--- database/seeds/DishSeeder.php
+++ database/seeds/DishSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $dishes = config("dishes-filler");
+
+
+        foreach ($dishes as $dish) {
+            $newDish = new Dish();
+            //
+            $newDish->name = $dish["name"];
+            $newDish->slug = $dish["slug"];
+            $newDish->price = $dish["price"];
+            $newDish->visible = $dish["visible"];
+            $newDish->description = $dish["description"];
+            $newDish->ingredients = $dish["ingredients"];
+            $newDish->image = $dish["image"];
+            $newDish->restaurant_id = $dish["restaurant_id"];
+            $newDish->course_id = $dish["course_id"];
+
+            //
+            $newDish->save();
+        }
     }
 }
