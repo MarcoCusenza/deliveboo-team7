4d3ee37bec9d0e7accc6d71ed64ee2b1a43fd9d0
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-03-13 15:40:38 +0100

    Push show test gabri

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
+<div class="container mx-auto">
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img width="500" class="card-img-top" src="{{asset("storage/{$restaurant->image}")}}" alt="{{$restaurant->restaurant_name}}">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">{{$restaurant->restaurant_name}}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
