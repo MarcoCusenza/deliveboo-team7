0d948df5326c1e5dd2cb75b0ab9d9c9eff0ad4a5
francesca <fradeluca85@gmail.com>
2022-03-13 17:01:04 +0100

    add show.blade di restaurant

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -2,13 +2,22 @@
 
 @section('content')
 <div class="container mx-auto">
-<div class="card" style="width: 18rem;">
-  <img width="500" class="card-img-top" src="{{asset("storage/{$restaurant->image}")}}" alt="{{$restaurant->restaurant_name}}">
-  <div class="card-body">
-    <h5 class="card-title">{{$restaurant->restaurant_name}}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="card mb-3">
+    <img width="400" class="rounded mt-3 mx-auto d-block" src="{{asset("storage/{$restaurant->image}")}}" alt="{{$restaurant->restaurant_name}}">
+    <div class="card-body">
+      <h5 class="card-title">Nome ristorante: <span class="font-weight-bold">{{$restaurant->restaurant_name}}</span></h5>
+      <div class="card" style="width: 18rem;">
+        <div class="card-header">
+          Info:
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Numero di telefono: {{$restaurant->phone}}</li>
+          <li class="list-group-item">Indirizzo: {{$restaurant->address}}</li>
+          <li class="list-group-item">Prezzo per la consegna: {{$restaurant->delivery_price}} euro</li>
+        </ul>
+      </div>
+    </div>
   </div>
-</div>
+  
 </div>
 @endsection
