9e105f6ef40857347b5a8d8da5ce32bc7d725527
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-03-13 15:33:44 +0100

    CRUD Restaurant + Crezione file .blade (INCOMPL)

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
 
+  // protected $fillable = ["restaurant_name", "phone", "address", "image", "delivery_price"];
+
   public function user()
   {
     return $this->belongsTo("App\User");
