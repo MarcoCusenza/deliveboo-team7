b61fa376b432fa7b784a4d1b09bc3f9606c02169
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-03-13 17:37:12 +0100

    Fix form, ma error. Le img vengono salvate

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -6,7 +6,7 @@
       @csrf
       <div class="form-group">
         <label for="restaurant_name">Nome ristorante</label>
-        <input type="text" class="form-control @error("restaurant_name") is-invalid @enderror" id="restaurant_name" placeholder="Scrivi il nome del ristorante" value="{{old("restaurant_name")}}">
+        <input type="text" class="form-control @error("restaurant_name") is-invalid @enderror" id="restaurant_name" name="restaurant_name" placeholder="Scrivi il nome del ristorante" value="{{old("restaurant_name")}}">
       </div>
       @error("restaurant_name")
         <div class="alert alert-danger">{{$message}}</div>
@@ -14,7 +14,7 @@
 
       <div class="form-group">
         <label for="phone">Numero di telefono</label>
-        <input type="text" class="form-control @error("phone") is-invalid @enderror" id="phone" placeholder="Scrivi il numero di telefono" value="{{old("phone")}}">
+        <input type="text" class="form-control @error("phone") is-invalid @enderror" id="phone" name="phone" placeholder="Scrivi il numero di telefono" value="{{old("phone")}}">
       </div>
       @error("phone")
         <div class="alert alert-danger">{{$message}}</div>
@@ -22,7 +22,7 @@
 
       <div class="form-group">
         <label for="address">Indirizzo</label>
-        <input type="text" class="form-control @error("phone") is-invalid @enderror" id="address" placeholder="Scrivi l'indirizzo" value="{{old("address")}}">
+        <input type="text" class="form-control @error("phone") is-invalid @enderror" id="address" name="address" placeholder="Scrivi l'indirizzo" value="{{old("address")}}">
       </div>
       @error("address")
         <div class="alert alert-danger">{{$message}}</div>
@@ -30,22 +30,22 @@
 
       <div class="form-group mb-4">
         <label for="delivery_price">Costo spedizione</label>
-        <input type="number" step=".05" class="form-control @error("phone") is-invalid @enderror" id="delivery_price" placeholder="Scrivi quanto costa la spedizione" value="{{old("delivery_price")}}">
+        <input type="number" step=".05" class="form-control @error("phone") is-invalid @enderror" id="delivery_price" name="delivery_price" placeholder="Scrivi quanto costa la spedizione" value="{{old("delivery_price")}}">
       </div>
       @error("delivery_price")
         <div class="alert alert-danger">{{$message}}</div>
       @enderror
 
-      <div class="form-group">
-        <label for="category">Seleziona una categoria</label>
-        <select class="custom-select @error("category_id") is-invalid @enderror" name="category_id" id="category">
-            <option></option>
-            @foreach ($categories as $category)
-                <option value="{{$category->id}}" {{old("category_id") == $category->id ? "selected" : ""}}>{{$category->name}}</option>
-            @endforeach
-        </select>
-        @error("category_id")
-            <div class="alert alert-danger">{{$message}}</div>
+      <div class="form-group mb-4">
+        <p>Seleziona le categorie</p>
+        @foreach ($categories as $category)
+            <div class="form-check form-check-inline">
+                <input type="checkbox" class="form-check-input" id="{{$category->slug}}" name="category[]" value="{{$category->id}}" {{in_array( $category->id, old("category", []) ) ? 'checked' : ''}}>
+                <label class="form-check-label" for="{{$category->slug}}">{{$category->name}}</label>
+            </div>
+        @endforeach
+        @error('category')
+            <div class="alert alert-danger">{{ $message }}</div>
         @enderror
       </div>
 
