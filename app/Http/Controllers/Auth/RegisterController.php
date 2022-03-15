634042eca8d224ff0e0c8fbcf98056b3a990969f
francesca <fradeluca85@gmail.com>
2022-03-15 10:32:09 +0100

    correzione register unique mail e vat

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'VAT_number' => ['required', 'string', 'max:255'],
+            'VAT_number' => ['required', 'string', 'max:255', 'unique:users'],
         ]);
     }
 
