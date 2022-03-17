5531f0dd7624d3c7df0b0847f91ea53fc7d7c50e
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-03-17 09:55:59 +0100

    specifica ristoratore form login/register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Accedi') }}</div>
+                    <div class="card-header">{{ __('Accedi come ristoratore') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
