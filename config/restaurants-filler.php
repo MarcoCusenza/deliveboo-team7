570befb11b6ebd97415d62efc98fe94fe6fb81f9
francesca <fradeluca85@gmail.com>
2022-03-17 15:23:26 +0100

    init filler change

--- config/restaurants-filler.php
+++ config/restaurants-filler.php
@@ -4,7 +4,7 @@ return [
   [
     "user_id" => 1,
     "restaurant_id" => 1,
-    "category_id" => [1],
+    "category_id" => [2, 8, 9,],
     "restaurant_name" => "Jotaro Sushi",
     "slug" => "jotaro-sushi",
     "phone" => "+393334445566",
@@ -15,7 +15,7 @@ return [
   [
     "user_id" => 2,
     "restaurant_id" => 2,
-    "category_id" => [8],
+    "category_id" => [6, 10,],
     "restaurant_name" => "Pizza in teglia da Mario",
     "slug" => "pizza-in-teglia-da-mario",
     "phone" => "+393339998877",
@@ -26,7 +26,7 @@ return [
   [
     "user_id" => 3,
     "restaurant_id" => 3,
-    "category_id" => [6],
+    "category_id" => [4, 8, 10],
     "restaurant_name" => "Al Noor",
     "slug" => "al-noor",
     "phone" => "+393485556677",
@@ -37,7 +37,7 @@ return [
   [
     "user_id" => 4,
     "restaurant_id" => 4,
-    "category_id" => [7],
+    "category_id" => [5, 8, 9, 10],
     "restaurant_name" => "Todo in Todos",
     "slug" => "todo-in-todos",
     "phone" => "+393358882244",
@@ -48,7 +48,7 @@ return [
   [
     "user_id" => 5,
     "restaurant_id" => 5,
-    "category_id" => [4],
+    "category_id" => [1, 8, 9],
     "restaurant_name" => "Calcio Champagne",
     "slug" => "calcio-champagne",
     "phone" => "+393357776655",
@@ -59,7 +59,7 @@ return [
   [
     "user_id" => 6,
     "restaurant_id" => 6,
-    "category_id" => [9],
+    "category_id" => [7, 8],
     "restaurant_name" => "Il Corner",
     "slug" => "il-corner",
     "phone" => "+393351112233",
@@ -67,4 +67,104 @@ return [
     "image" => "uploads/rest6.jpg",
     "delivery_price" => 2.50
   ],
+  [
+    "user_id" => 7,
+    "restaurant_id" => 7,
+    "category_id" => [1, 8, 9, 10, 6],
+    "restaurant_name" => "Il Giardino",
+    "slug" => "il-giardino",
+    "phone" => "+393409876234",
+    "address" => "via Livorno, 54",
+    "image" => "uploads/rest7.jpg",
+    "delivery_price" => 4.50
+  ],
+  [
+    "user_id" => 8,
+    "restaurant_id" => 8,
+    "category_id" => [6, 10, 11, 12],
+    "restaurant_name" => "Pizza Pazza",
+    "slug" => "pizza-pazza",
+    "phone" => "+393478903456",
+    "address" => "Piazza Italia,23",
+    "image" => "uploads/rest8.jpg",
+    "delivery_price" => 4.00
+  ],
+  [
+    "user_id" => 9,
+    "restaurant_id" => 9,
+    "category_id" => [2, 3, 8, 9, 10],
+    "restaurant_name" => "Dumpling Restaurant",
+    "slug" => "dumpling-restaurant",
+    "phone" => "+393356709176",
+    "address" => "Largo Ventura,103",
+    "image" => "uploads/rest9.jpg",
+    "delivery_price" => 5.00
+  ],
+  [
+    "user_id" => 10,
+    "restaurant_id" => 10,
+    "category_id" => [2, 1, 6, 8, 9, 10],
+    "restaurant_name" => "Grand Dragon",
+    "slug" => "grand-dragon",
+    "phone" => "+393309843678",
+    "address" => "via Della Spina,13",
+    "image" => "uploads/rest10.jpg",
+    "delivery_price" => 3.50
+  ],
+  [
+    "user_id" => 11,
+    "restaurant_id" => 11,
+    "category_id" => [5, 1, 6, 8, 9, 10],
+    "restaurant_name" => "Bodega Sánchez",
+    "slug" => "bodega-sanchez",
+    "phone" => "+393376589023",
+    "address" => "Largo Cesare,78",
+    "image" => "uploads/rest11.jpg",
+    "delivery_price" => 4.90
+  ],
+  [
+    "user_id" => 12,
+    "restaurant_id" => 12,
+    "category_id" => [1, 2, 9, 10, 12],
+    "restaurant_name" => "Il codino",
+    "slug" => "il-codino",
+    "phone" => "+393307690654",
+    "address" => "via Firenze, 100",
+    "image" => "uploads/rest12.jpg",
+    "delivery_price" => 5.00
+  ],
+  [
+    "user_id" => 13,
+    "restaurant_id" => 13,
+    "category_id" => [6, 7, 8, 10, 11, 12],
+    "restaurant_name" => "PaniPizza",
+    "slug" => "panipizza",
+    "phone" => "+393356751234",
+    "address" => "via Catanzaro, 89",
+    "image" => "uploads/rest13.jpg",
+    "delivery_price" => 3.00
+  ],
+  [
+    "user_id" => 14,
+    "restaurant_id" => 14,
+    "category_id" => [1, 3, 9, 12],
+    "restaurant_name" => "Acqua pazza",
+    "slug" => "acqua-pazza",
+    "phone" => "+393398765351",
+    "address" => "via dell'Olmo, 89",
+    "image" => "uploads/rest14.jpg",
+    "delivery_price" => 6.00
+  ],
+  [
+    "user_id" => 15,
+    "restaurant_id" => 15,
+    "category_id" => [4, 1],
+    "restaurant_name" => "Bombay",
+    "slug" => "bambay",
+    "phone" => "+393345609817",
+    "address" => "viale Italia, 109",
+    "image" => "uploads/rest15.jpg",
+    "delivery_price" => 3.90
+  ],
+
 ];
