4f6ee51da5797a8671637331ed5e7f497a42cd3e
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-03-18 11:42:41 +0100

    init frontend scaffolding

---
+++ resources/js/guest/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <MainHomeSection />
+    <Work />
+  </main>
+</template>
+
+<script>
+import MainHomeSection from "../components/sections/home/MainHomeSection.vue";
+import Work from "../components/sections/home/Work.vue";
+
+export default {
+  name: "Main",
+  components: {
+    MainHomeSection,
+    Work,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/guest/pages/PageNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container my-5 py-5">
+    <h1>La pagina non è stata trovata</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
