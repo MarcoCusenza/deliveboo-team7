ef10b8b209f225868a8b408adbb4f3863b05ee7c
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-03-20 13:05:27 +0100

    Fix restaurant.image front-end

--- resources/js/guest/pages/Restaurant.vue
+++ resources/js/guest/pages/Restaurant.vue
@@ -1,41 +1,50 @@
 <template>
-  <div class="container">
-    <img :src="'../storage/' + restaurant.image" :alt="restaurant.name" />
-    <h1 class="restaurant-name">{{ restaurant.restaurant_name }}</h1>
+    <div class="container">
+        <img v-if="restaurant.image && isFirstLetterH(restaurant.image)" :src="restaurant.image" :alt="restaurant.name"/>
+        <img v-else :src="'../storage/' + restaurant.image" :alt="restaurant.name"/>
+        <h1 class="restaurant-name">{{ restaurant.restaurant_name }}</h1>
 
-    <!-- Lista dei piatti -->
-    <DishList />
-  </div>
+        <!-- Lista dei piatti -->
+        <DishList />
+    </div>
 </template>
 
 <script>
-import DishList from "../components/sections/restaurant/DishList.vue";
+    import DishList from "../components/sections/restaurant/DishList.vue";
 
-export default {
-  name: "Restaurant",
-  components: {
-    DishList,
-  },
-  data() {
-    return {
-      restaurant: {},
-    };
-  },
-  created() {
-    axios
-      .get(`/api/restaurant/${this.$route.params.slug}`)
-      .then((response) => {
-        this.restaurant = response.data;
-      })
-      .catch((error) => {
-        this.$router.push({ name: "page-404" });
-      });
-  },
-};
+    export default {
+        name: "Restaurant",
+        components: {
+            DishList,
+        },
+        data() {
+            return {
+                restaurant: {},
+            };
+        },
+        methods: {
+            isFirstLetterH(item) {
+                console.log(item)
+                return item[0] == 'h'
+            },
+        },
+        created() {
+            axios
+                .get(`/api/restaurant/${this.$route.params.slug}`)
+                .then((response) => {
+                    this.restaurant = response.data;
+                })
+                .catch((error) => {
+                    this.$router.push({
+                        name: "page-404"
+                    });
+                });
+        },
+    }
 </script>
 
 <style lang="scss" scoped>
-img {
-  max-width: 300px;
-}
+    img {
+        max-width: 300px;
+    }
 </style>
